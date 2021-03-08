0a82e5b60955af30ba3e3276c290419e9d3d3e3a
manuelceli <celiyelamc123@gmail.com>
2021-03-07 20:24:27 -0500

    inicio

--- app/src/main/java/com/example/pi8_turnos/TurnosProgramadosActivity.java
+++ app/src/main/java/com/example/pi8_turnos/TurnosProgramadosActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.pi8_turnos.Model.PreTurnos;
@@ -28,6 +30,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TurnosProgramadosActivity extends AppCompatActivity {
 
 
@@ -35,6 +42,10 @@ public class TurnosProgramadosActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
     String fecha, idInstitucion;
+    String horasInicioT = "", minutosInicioT = "";
+    String horasFinT = "", minutosFinT = "";
+    private int horasInicio, minutosInicio;
+    private int horasFin, minutosFin;
     private String id;
 
     @Override
@@ -95,9 +106,10 @@ public class TurnosProgramadosActivity extends AppCompatActivity {
                                                         String idTurno = getRef(position).getKey();
                                                         //Toast.makeText(ListaRecordatoriosActivity.this,id, Toast.LENGTH_SHORT).show();
                                                         AlertDialog.Builder builder = new AlertDialog.Builder(TurnosProgramadosActivity.this);
-                                                        CharSequence[] items = new CharSequence[2];
+                                                        CharSequence[] items = new CharSequence[3];
                                                         items[0] = "Eliminar Turno";
-                                                        items[1] = "Cancelar";
+                                                        items[1] = "Modificar";
+                                                        items[2] = "Cancelar";
                                                         builder.setItems(items, new DialogInterface.OnClickListener() {
                                                             @Override
                                                             public void onClick(DialogInterface dialogInterface, int i) {
@@ -114,6 +126,154 @@ public class TurnosProgramadosActivity extends AppCompatActivity {
                                                                             }
                                                                         }
                                                                     });
+                                                                } else if (i == 1) {
+                                                                    AlertDialog.Builder mBuilder = new AlertDialog.Builder(TurnosProgramadosActivity.this);
+                                                                    View mView = getLayoutInflater().inflate(R.layout.dialog_entretiempo, null);
+                                                                    mBuilder.setTitle("Modificar turno");
+                                                                    mBuilder.setCancelable(false);
+                                                                    TextView inicioEntretiempo = (TextView) mView.findViewById(R.id.inicioEntretiempo);
+                                                                    TextView finEntretiempo = (TextView) mView.findViewById(R.id.finEntretiempo);
+                                                                    inicioEntretiempo.setText(model.getHora_inicio());
+                                                                    finEntretiempo.setText(model.getHora_fin());
+
+                                                                    /*if (!horasInicioT.equals("")) {
+                                                                        horasInicio = Integer.valueOf(horasInicioT);
+                                                                        minutosInicio = Integer.valueOf(minutosInicioT);
+                                                                        inicioEntretiempo.setText(horasInicioT + ":" + minutosInicioT);
+                                                                    }
+
+
+                                                                    if (!horasFinT.equals("")) {
+                                                                        horasFin = Integer.valueOf(horasFinT);
+                                                                        minutosFin = Integer.valueOf(minutosFinT);
+                                                                        finEntretiempo.setText(horasFinT + ":" + minutosFinT);
+                                                                    }*/
+
+                                                                    mBuilder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                                                        @Override
+                                                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                                                            try {
+                                                                                SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy HH:mm:ss");
+                                                                                String inicioInString = fecha + " " + inicioEntretiempo.getText().toString() + ":00";
+                                                                                Date inicio = sdf.parse(inicioInString);
+
+                                                                                String finInString = fecha + " " + finEntretiempo.getText().toString() + ":00";
+                                                                                Date fin=sdf.parse(finInString);
+
+                                                                                String com = String.valueOf(fin.compareTo(inicio));
+
+                                                                                if (com.equals("1")) {
+
+                                                                                    FirebaseDatabase.getInstance().getReference().child("Turnos").child(id).child("horario").child(idTurno).child("hora_inicio").setValue(inicioEntretiempo.getText().toString());
+                                                                                    FirebaseDatabase.getInstance().getReference().child("Turnos").child(id).child("horario").child(idTurno).child("hora_fin").setValue(finEntretiempo.getText().toString());
+                                                                                    Toast.makeText(TurnosProgramadosActivity.this, "Turno Modificado", Toast.LENGTH_SHORT).show();
+
+
+                                                                                }else {
+                                                                                        Toast.makeText(TurnosProgramadosActivity.this, "La hora de fin debe ser mayor a la de inicio", Toast.LENGTH_SHORT).show();
+                                                                                    }
+
+                                                                            }catch(Exception e){
+
+                                                                            }
+
+                                                                            /*FirebaseDatabase.getInstance().getReference()
+                                                                                    .child("Turnos").child(id).child("horario").child(idTurno).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    if (task.isSuccessful()) {
+                                                                                        Toast.makeText(TurnosProgramadosActivity.this, "Turno Eliminado", Toast.LENGTH_SHORT).show();
+                                                                                    } else {
+                                                                                        Toast.makeText(TurnosProgramadosActivity.this, "Remove Failed", Toast.LENGTH_SHORT).show();
+                                                                                    }
+                                                                                }
+                                                                            });*/
+
+
+
+
+                                                                          /*if (horasInicioEntretiempo.length() > 0 && horasFinEntretiempo.length() > 0) {
+                                                                                tvEntretiempo.setText(horasInicioEntretiempo + ":" + minutosInicioEntretiempo + " - " + horasFinEntretiempo + ":" + minutosFinEntretiempo);
+                                                                                dialogInterface.dismiss();
+                                                                            } else {
+                                                                                Toast.makeText(mView.getContext(), "Enter values", Toast.LENGTH_SHORT).show();
+                                                                            }*/
+                                                                        }
+                                                                    });
+                                                                    mBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                                                        @Override
+                                                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                                                        }
+                                                                    });
+                                                                    mBuilder.setView(mView);
+                                                                    AlertDialog dialog = mBuilder.create();
+                                                                    dialog.show();
+
+                                                                   /* if (!horasInicioT.equals("")) {
+                                                                        horasInicio = Integer.valueOf(horasInicioT);
+                                                                        minutosInicio= Integer.valueOf(minutosInicioT);
+                                                                        inicioEntretiempo.setText(horasInicioT + ":" + minutosInicioT);
+                                                                    }
+
+
+                                                                    if (!horasFinT.equals("")) {
+                                                                        horasFin = Integer.valueOf(horasFinT);
+                                                                        minutosFin = Integer.valueOf(minutosFinT);
+                                                                        finEntretiempo.setText(horasFinT + ":" + minutosFinT);
+                                                                    }*/
+
+                                                                    horasInicio=Integer.valueOf(inicioEntretiempo.getText().toString().substring(0,2));
+                                                                    minutosInicio=Integer.valueOf(inicioEntretiempo.getText().toString().substring(3,5));
+
+                                                                    horasFin=Integer.valueOf(finEntretiempo.getText().toString().substring(0,2));
+                                                                    minutosFin=Integer.valueOf(finEntretiempo.getText().toString().substring(3,5));
+
+                                                                    inicioEntretiempo.setOnClickListener(new View.OnClickListener() {
+                                                                        @Override
+                                                                        public void onClick(View view) {
+                                                                            TimePickerDialog ingresarHoraInicio = new TimePickerDialog(TurnosProgramadosActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                                                                                @Override
+                                                                                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                                                                    //Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(i), Toast.LENGTH_SHORT);
+                                                                                    horasInicioT = Integer.toString(i);
+                                                                                    minutosInicioT = Integer.toString(i1);
+                                                                                    if (horasInicioT.length() == 1)
+                                                                                        horasInicioT = "0" + horasInicioT;
+                                                                                    if (minutosInicioT.length() == 1)
+                                                                                        minutosInicioT = "0" + minutosInicioT;
+
+                                                                                    inicioEntretiempo.setText(horasInicioT + ":" + minutosInicioT);
+                                                                                }
+                                                                            }, horasInicio, minutosInicio, false);
+                                                                            ingresarHoraInicio.setTitle("Seleccione hora de inicio");
+                                                                            ingresarHoraInicio.show();
+                                                                        }
+                                                                    });
+
+                                                                    finEntretiempo.setOnClickListener(new View.OnClickListener() {
+                                                                        @Override
+                                                                        public void onClick(View view) {
+                                                                            TimePickerDialog ingresarHoraInicio = new TimePickerDialog(TurnosProgramadosActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                                                                                @Override
+                                                                                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                                                                    //Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(i), Toast.LENGTH_SHORT);
+                                                                                    horasFinT = Integer.toString(i);
+                                                                                    minutosFinT = Integer.toString(i1);
+                                                                                    if (horasFinT.length() == 1)
+                                                                                        horasFinT = "0" + horasFinT;
+                                                                                    if (minutosFinT.length() == 1)
+                                                                                        minutosFinT = "0" + minutosFinT;
+
+                                                                                    finEntretiempo.setText(horasFinT + ":" + minutosFinT);
+                                                                                }
+                                                                            }, horasFin, minutosFin, false);
+                                                                            ingresarHoraInicio.setTitle("Seleccione hora de inicio");
+                                                                            ingresarHoraInicio.show();
+                                                                        }
+                                                                    });
+
                                                                 }
                                                             }
                                                         });
